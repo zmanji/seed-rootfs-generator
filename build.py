47dfacd1b60d38cf04a31f0da11182d85512b6be
Zameer Manji <zmanji@gmail.com>
2022-05-15 18:31:08 +0000

    ensure cache can be saved

--- build.py
+++ build.py
@@ -42,6 +42,8 @@ def main():
                 "--setup-hook=mkdir -p \"$1\"/var/cache/apt/archives/",
                 "--setup-hook=copy-in " + str(deb_cache) + " /var/cache/apt/archives/",
                 "--customize-hook=copy-out /var/cache/apt/archives " + str(deb_cache),
+                "--customize-hook=rm " + str(deb_cache / "partial"),
+                "--customize-hook=rm " + str(deb_cache / "lock"),
                 # end machinery
                 "--variant=buildd",
                 "--mode=unshare",
