7b3576a9f87c5b5797d62d121d79c26e818de382
Zameer Manji <zmanji@gmail.com>
2022-05-15 20:43:13 +0000

    try fixing cache issues

--- build.py
+++ build.py
@@ -45,8 +45,9 @@ def main():
                 "--setup-hook=ls -lah \"$1\"/var/cache/apt/archives/",
                 "--customize-hook=rm -rf " + str(deb_cache) +  "/* && " + " cp \"$1\"/var/cache/apt/archives/*.deb " + str(deb_cache),
                 # end machinery
-                "--variant=buildd",
-                "--include=python3,cmake,ninja-build,ca-certificates",
+                "--variant=custom",
+                "--include=build-essential,python3,cmake,ninja-build,ca-certificates",
+                "--include=?essential",
                 "--hook-dir=./mmdebstrap/hooks/eatmydata",
                 "bullseye",
                 tfile,
